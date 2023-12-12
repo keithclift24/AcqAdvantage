5180fe4475b08624e25452454c199c5bd298a4a9
Keith Clift <keithclift24@gmail.com>
2023-12-11 20:30:23 -0600

    build

--- acqadvantage/build/web/flutter_service_worker.js
+++ acqadvantage/build/web/flutter_service_worker.js
@@ -9,7 +9,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0307c6ebb9dd732f584e10adbc73ed94
 "assets/assets/images/acqprologo.png": "65b93e92b4af9a192964eb65b45acf49",
 "assets/assets/images/acqprologooriginal.png": "83b0305ee2d175c5358b5957e0a8d7de",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "70b2f884921950609f64d8cb7e5f97c6",
+"assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
 "assets/NOTICES": "ece4a16659fed1a0c132ef75cc1bd3b8",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "4096b5150bac93c41cbc9b45276bd90f",
@@ -26,9 +26,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0307c6ebb9dd732f584e10adbc73ed94
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f04c3c39ff3ac60ce634f726bdfa47cf",
-"/": "f04c3c39ff3ac60ce634f726bdfa47cf",
-"main.dart.js": "ba79892cea77348af72d9a96a1f9fd97",
+"index.html": "d3d84fd7045d870ad6dd2247a1d761ca",
+"/": "d3d84fd7045d870ad6dd2247a1d761ca",
+"main.dart.js": "5c02ee61fd551c8eb200e587351fbf99",
 "manifest.json": "49fc1181233f4287db1dc20d3fa88076",
 "version.json": "76ee6c16d2212f4552809b05c25b9928"};
 // The application shell files that are downloaded before a service worker can
